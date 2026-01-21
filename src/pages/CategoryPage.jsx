70682bcc0ec7624043995e7e25ba40fe21db9591
rahna878 <rahnak235@gmail.com>
2026-01-21 21:59:53 +0530

    Standardize image loading on MyOrdersPage

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -46,7 +46,7 @@ const CategoryPage = () => {
                             <Link to={`/product/${product.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
                                 <div style={imageWrapper}>
                                     <img
-                                        src={product.thumbnail ? `https://rahna.pythonanywhere.com${item.product?.thumbnail}` : "/no-image.png"}
+                                        src={product.thumbnail ? `https://rahna.pythonanywhere.com${product.thumbnail}` : "/no-image.png"}
                                         alt={product.title}
                                         style={imgStyle}
                                     />
